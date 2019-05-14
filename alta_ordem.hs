77d8b2f1a79946cad7dc47e1c5182f80c0865a34
kevin-weitgenant <kevin.weitgenant@gmail.com>
2019-05-14 18:22:33 -0300

    Update alta_ordem.hs

--- alta_ordem.hs
+++ alta_ordem.hs
@@ -1,6 +1,5 @@
-
 times::Int->Int
-times x  = x*2
+times x  = 2*x
 
 mapInt:: (Int->Int) -> [Int]->[Int]
 mapInt f [] = []
@@ -9,7 +8,7 @@ mapInt f (a:x) = f a : mapInt f x
 --total:: (a->b)->a->b
 
 
-foldInt :: (Int -> Int -> Int) -> [Int] -> Int --revisar	
+foldInt :: (Int -> Int -> Int) -> [Int] -> Int 	
 foldInt f [a] = a
 foldInt f (a:x) = f a (foldInt f x)
 
@@ -23,11 +22,49 @@ filterString f (a:x)
 	|f a = a: filterString f x
 	|otherwise = filterString f x
 
-naoEspaco :: Char -> Bool
+
+
+naoEspaco ::Char -> Bool
 naoEspaco x = x /= ' '
 
---quadrado::(Int->Int)->(Int->Int->Int)->[Int]->Int
---mapInt foldInt l = mapInt 
+soma_do_quadrado::[Int]->Int
+somado_do_quadrado[] = 0
+soma_do_quadrado l = foldInt soma (mapInt times l)
+
+
+testa_funcao::(Int->Int)->Int->Bool
+testa_funcao f 0 = True
+testa_funcao f n 
+	|(f n) > 0 = testa_funcao f (n-1)
+	|otherwise = False
+
+duasVezes::(Int->Int)->Int->Int
+duasVezes f x = f (f x)
+
+inter::Int->(Int->Int)->Int->Int
+inter 0 f x = f x
+inter n f x = f (inter (n-1) f x) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
